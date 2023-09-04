2fa7903b6d4d5692e5dd2371f2b6df0482fb79c9
ske1620 <dltmdals1620@gmail.com>
2023-09-04 15:58:25 +0900

    Chmod: 스프링부트 초기 환경 세팅

---
+++ back/dream/src/main/java/com/dream/dream/DreamApplication.java
@@ -0,0 +1,13 @@
+package com.dream.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamApplication.class, args);
+	}
+
+}
---
+++ back/dream/src/test/java/com/dream/dream/DreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
