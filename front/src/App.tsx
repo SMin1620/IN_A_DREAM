0bc04d0aa54ace2b13c49c83a54b44412f009c17
윤자현 <bajh323@naver.com>
2023-09-06 14:30:44 +0900

    feat: 라우터 설치

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import AppRouter from "./AppRouter";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
