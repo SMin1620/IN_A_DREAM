10a02f1ebd0136db8ac3e3931873c23f492f42c6
김동현 <kdhdh001@naver.com>
2023-09-06 14:37:43 +0900

    Update front/src/pages/LoginPage.tsx, front/src/AppRouter.tsx

--- front/src/AppRouter.tsx
+++ front/src/AppRouter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import IntroPage from "./pages/IntroPage";
+import LoginPage from "./pages/LoginPage";
 
 function AppRouter() {
   return (
@@ -10,6 +11,7 @@ function AppRouter() {
         <Routes>
           <Route path="/Main" element={<MainPage />}></Route>
           <Route path="/Intro" element={<IntroPage />}></Route>
+          <Route path="/Login" element={<LoginPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
