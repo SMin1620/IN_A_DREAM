f6cd45fe9a3e6c18ff89e8c4e7307d9ad7a6cb0a
윤자현 <bajh323@naver.com>
2023-09-06 14:33:21 +0900

    feat: Create IntroPage

--- front/src/AppRouter.tsx
+++ front/src/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import IntroPage from "./pages/IntroPage";
 
 function AppRouter() {
   return (
@@ -8,6 +9,7 @@ function AppRouter() {
       <BrowserRouter>
         <Routes>
           <Route path="/Main" element={<MainPage />}></Route>
+          <Route path="/Intro" element={<IntroPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
