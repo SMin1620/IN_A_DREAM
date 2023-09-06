ad0213109c9fe676efcafa8fed9d276501223656
이승민 <dltmdals1620@gmail.com>
2023-09-06 17:43:37 +0900

    feat: 카프카 프로듀서, 컨슈머 설정 및 api

--- back/dream/src/main/java/com/dream/dream/kafka/LogDto.java
+++ back/dream/src/main/java/com/dream/dream/kafka/LogDto.java
@@ -1,4 +1,16 @@
 package com.dream.dream.kafka;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class LogDto {
+    private LocalDateTime localDateTime;
+    private String message;
 }
---
+++ back/dream/src/main/java/com/dream/dream/kafka/service/KafkaConsumerService.java
@@ -0,0 +1,30 @@
+package com.dream.dream.kafka.service;
+
+import com.dream.dream.kafka.LogDto;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "${message.topic.name}", groupId = ConsumerConfig.GROUP_ID_CONFIG, containerFactory = "logDtoListener")
+
+    @Async
+    public void consume(LogDto logDto) throws IOException {
+        //System.out.println("Consumed msg : "+message.toString());
+
+//        LogDto dto = new LogDto();
+//        dto.setLocalDateTime(LocalDateTime.now());
+//        dto.setMessage("테스트 로그 데이터");
+
+        System.out.println(logDto.getLocalDateTime().toString());
+
+    }
+}
