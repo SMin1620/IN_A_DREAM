d178a03edb1f49fdff25db2ac79abd3325e366eb
이승민 <dltmdals1620@gmail.com>
2023-09-08 16:22:20 +0900

    feat: logDto 포매팅

--- back/dream/src/main/java/com/dream/dream/kafka/service/KafkaProducerService.java
+++ back/dream/src/main/java/com/dream/dream/kafka/service/KafkaProducerService.java
@@ -1,6 +1,7 @@
 package com.dream.dream.kafka.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaProducerService {
 
     @Value(value = "${message.topic.name}")
@@ -18,12 +20,6 @@ public class KafkaProducerService {
 
     @Async
     public void sendLogDto(Object message){
-        /*try {
-            Thread.sleep(5000);*/
-        //System.out.println("Produce content : "+message.getContent());
-        kafkaTemplate.send(topicName,message);
-        /*}catch (InterruptedException e){
-            e.printStackTrace();
-        }*/
+        kafkaTemplate.send(topicName, message);
     }
 }
