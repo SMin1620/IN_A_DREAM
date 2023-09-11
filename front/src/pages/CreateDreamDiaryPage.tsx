a03d36aa160913e43be9a0b1e4ae056ab68f68e8
윤자현 <bajh323@naver.com>
2023-09-11 15:00:06 +0900

    feat: Create Btn

--- front/src/pages/CreateDreamDiaryPage.tsx
+++ front/src/pages/CreateDreamDiaryPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Button from "../components/common/Button";
+import Input from "../components/common/Input";
 const CreateDreamDiaryPage = () => {
+  const inputTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
   return (
     <div>
-      CreateDreamDiary
-      <Button></Button>
+      오늘의 꿈 일기를 작성해보아요.
+      <Button top={20} right={10} position="absolute"></Button>
+      <Input
+        type="text"
+        placeholder="일기 제목을 입력하세요."
+        onChange={(e) => inputTitle(e)}
+      />
     </div>
   );
 };
