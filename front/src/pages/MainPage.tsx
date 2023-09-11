6f7ba58775c813912ece2a7f684664600be58b5f
김동현 <kdhdh001@naver.com>
2023-09-11 16:23:21 +0900

    feat: 글꼴 추가

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import NavBar from "../components/features/Navbar";
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <NavBar />
+      소개 프로젝트
+    </div>
+  );
 };
 
 export default MainPage;
