aad2d44936d3e6052f8206d393ab06fbcd0f80e4
이승민 <dltmdals1620@gmail.com>
2023-09-13 16:20:56 +0900

    feat: swagger build 추가

--- back/dream/src/main/java/com/dream/dream/kafka/controller/KafkaController.java
+++ back/dream/src/main/java/com/dream/dream/kafka/controller/KafkaController.java
@@ -25,57 +25,21 @@ public class KafkaController {
     /**
      * 테스트 로그 생성 컨트롤러
      */
-//    @PostMapping(value = "/message")
-//    public LogDto sendMessage(@RequestBody LogDto logDto){
-//
-//        // 로그 생성 날짜
-//        logDto.setLocalDateTime(LocalDateTime.now());
-//        logDto.setDiaryId(1L);
-//        logDto.setContent("멍청이 바보");
-//        logDto.setPositive(99.1F);
-//        logDto.setNeutral(0.02F);
-//        logDto.setNegative(0.88F);
-//        logDto.setLike(10);
-//        kafkaProducerService.sendLogDto(logDto);
-//        return logDto;
-//    }
-
-    /**
-     * 테스트 로그 생성 컨트롤러
-     */
     @PostMapping(value = "/message")
     public PointHistoryDto sendMessage(@RequestBody PointHistoryDto pointHistoryDto){
 
         // 로그 생성 날짜
-//        pointHistoryDto.setBuyer(1L);
-//        pointHistoryDto.setSeller(22L);
-//        pointHistoryDto.setPoint(50);
+        pointHistoryDto.setBuyer(1L);
+        pointHistoryDto.setSeller(22L);
+        pointHistoryDto.setPoint(1150);
 
         LocalDateTime currentTime = LocalDateTime.now();
+        pointHistoryDto.setTimestamp(currentTime);
 
-        PointHistoryDto data = new PointHistoryDto();
 
-        int idx = 1;
-        for (int i = 0; i < 40; i++) {
-            data.setPoint(idx);
-            data.setSeller((long) idx);
-            data.setTimestamp(currentTime);
 
-            // 2분씩 시간 차이 추가
-            currentTime = currentTime.minus(2, ChronoUnit.MINUTES);
+        kafkaProducerService.sendPointLogDto(pointHistoryDto);
 
-            // 원하는 포맷 지정
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy @ HH:mm:ss.SSS");
-//            data.setTimestamp(currentTime.format(formatter));
-
-            data.setTimestamp(currentTime);
-
-            kafkaProducerService.sendPointLogDto(data);
-            idx++;
-        }
-
-//        kafkaProducerService.sendPointLogDto(pointHistoryDto);
-
-        return data;
+        return pointHistoryDto;
     }
 }
