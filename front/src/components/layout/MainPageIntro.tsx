7a2d703a914c3367edaa16b6547392202087fa66
김동현 <kdhdh001@naver.com>
2023-09-13 12:43:14 +0900

    feat: MainPageintro 완성

--- front/src/components/layout/MainPageIntro.tsx
+++ front/src/components/layout/MainPageIntro.tsx
@@ -1,36 +1,74 @@
 import React from "react";
 import ImgCard from "../common/imgCard";
 import CardDeck from "../features/cardDeck";
+import styled from "styled-components";
+
+const images = [
+  "/card/background1.png",
+  "/card/dff.png",
+  "/card/ggum3.jpg",
+  "/card/igi4_tam5_210723.jpg",
+  "/card/background1.png",
+  "/card/dff.png",
+  "/card/ggum3.jpg",
+  "/card/igi4_tam5_210723.jpg",
+  "/card/background1.png",
+  "/card/dff.png",
+  "/card/ggum3.jpg",
+  "/card/igi4_tam5_210723.jpg",
+  "/card/background1.png",
+  "/card/dff.png",
+  "/card/ggum3.jpg",
+  "/card/igi4_tam5_210723.jpg",
+  "/card/background1.png",
+  "/card/dff.png",
+  "/card/ggum3.jpg",
+  "/card/igi4_tam5_210723.jpg",
+  "/card/background1.png",
+  "/card/dff.png",
+  "/card/ggum3.jpg",
+  "/card/igi4_tam5_210723.jpg",
+  "/card/background1.png",
+  "/card/dff.png",
+  "/card/ggum3.jpg",
+  "/card/igi4_tam5_210723.jpg",
+  "/card/ggum3.jpg",
+  "/card/igi4_tam5_210723.jpg",
+];
+
+const MainPageIntroDiv = styled.div`
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const H1 = styled.h1`
+  color: white;
+  position: absolute;
+`;
+
+const CardDeckWrapper = styled.div`
+  position: relative;
+  z-index: 2;
+`;
 
 const MainPageIntro = () => {
   return (
-    <div>
-      <ImgCard
-        image="/card/dff.png"
-        width="8rem"
-        height="16rem"
-        border="3px solid black"
-        borderRadius="20px"
-      />
-      <CardDeck
-        images={[
-          "/card/background1.png",
-          "/card/dff.png",
-          "/card/ggum3.jpg",
-          "/card/igi4_tam5_210723.jpg",
-          "/card/background1.png",
-          "/card/dff.png",
-          "/card/ggum3.jpg",
-        ]}
-        imgCardProps={{
-          width: "8rem",
-          height: "16rem",
-          border: "3px solid black",
-          borderRadius: "20px",
-        }}
-      />
-    </div>
+    <MainPageIntroDiv>
+      <H1>In A DREAM, In A DREAM, In A DREAM</H1>
+      <CardDeckWrapper>
+        <CardDeck
+          images={images}
+          imgCardProps={{
+            width: "8.1rem",
+            height: "8.6rem",
+            borderRadius: "10px",
+          }}
+        />
+      </CardDeckWrapper>
+    </MainPageIntroDiv>
   );
 };
-
 export default MainPageIntro;
