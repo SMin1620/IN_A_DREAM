3b5233908dd10b0b3a8c4b99563493d65c5b1a92
이승민 <dltmdals1620@gmail.com>
2023-09-13 17:24:50 +0900

    feat: 꿈 일기 엔티티 설계

---
+++ back/dream/src/main/java/com/dream/dream/diary/entity/Diary.java
@@ -0,0 +1,57 @@
+package com.dream.dream.diary.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "feeds")
+public class Diary {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "diary_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private float positive = 0F;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private float neutral = 0F;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private float negative = 0F;
+
+    @Builder.Default
+    private int like = 0;
+
+    @Column(nullable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    private LocalDateTime localDateTime;
+
+//    private Member member;
+    private int member;
+
+//    private Emotion emotion;
+    private int emotion;
+
+}
