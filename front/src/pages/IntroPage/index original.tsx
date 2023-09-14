d97e39fae80083052077878a7d1840a8a05ab34d
HB_Kim <ghkdwp129@gmail.com>
2023-09-14 17:01:31 +0900

    feat:intro update

--- front/src/pages/IntroPage/index original.tsx
+++ front/src/pages/IntroPage/index original.tsx
@@ -2,8 +2,6 @@ import "./index.css";
 import React, { useEffect, useRef } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import bgImage from "../../assets/background/blackBG.jpg";
-import moonImg from "../../assets/image/moon.png";
-import castleImg from "../../assets/image/castle.png";
 // import landingImg from "../assets/image/landing.png";
 
 // styled-component 정의는 컴포넌트 외부에 위치해야 합니다.
@@ -14,23 +12,6 @@ const BackGround = styled.div`
   width: 100vw;
   z-index: -1;
   position: absolute;
-`;
-
-const Wrap = styled.div``;
-
-const Moon = styled.img.attrs({
-  src: moonImg,
-})`
-  height: 100%;
-  z-index: 1;
-`;
-const Castle = styled.img.attrs({
-  src: castleImg,
-})`
-  height: 100%;
-  bottom: 0;
-  align: center;
-  z-index: 1;
 `;
 
 const GlobalStyle = createGlobalStyle`
@@ -89,43 +70,42 @@ const IntroPage = () => {
   return (
     <>
       <GlobalStyle />
-      <Wrap>
-        <BackGround />
-        <div id="intro-main">
-          <p>IN</p>
-          <p>A</p>
-          <p>DREAM</p>
-          <p id="join-us-text">Join us!</p>
-          <img
-            id="down-arrow-icon"
-            src="./down-arrow.png"
-            alt="down arrow icon"
-          ></img>
-        </div>
-        <ul id="list-item-wrapper" ref={listItemWrapperRef}>
-          <li className="list-item">당신은 어떤 꿈을 꾸셨나요?</li>
-          <li className="list-item">
-            아름다운 꿈이었나요, 아니면 조금 무서웠나요?
-          </li>
-          <li className="list-item">당신의 꿈은 당신만의 이야기입니다.</li>
-          <li className="list-item">
-            그 이야기를 우리와 함께 나누어 보시겠어요?
-          </li>
-          <li className="list-item">지금 바로 당신의 꿈일기를 시작하세요!</li>
-          <li className="list-item">
-            여러분의 독특한 이야기가 다른 사람들에게도 도움이 될 수 있습니다.
-          </li>
-        </ul>
-        {/* <Impimg src={landingImg} alt="img" /> */}
-        {/* <Moon /> */}
-        {/* <Castle /> */}
-        {/* <Intro>
+
+      <BackGround />
+      <div id="intro-main">
+        <p>IN</p>
+        <p>A</p>
+        <p>DREAM</p>
+        <p id="join-us-text">Join us!</p>
+        <img
+          id="down-arrow-icon"
+          src="./down-arrow.png"
+          alt="down arrow icon"
+        ></img>
+      </div>
+      <ul id="list-item-wrapper" ref={listItemWrapperRef}>
+        <li className="list-item">당신은 어떤 꿈을 꾸셨나요?</li>
+        <li className="list-item">
+          아름다운 꿈이었나요, 아니면 조금 무서웠나요?
+        </li>
+        <li className="list-item">당신의 꿈은 당신만의 이야기입니다.</li>
+        <li className="list-item">
+          그 이야기를 우리와 함께 나누어 보시겠어요?
+        </li>
+        <li className="list-item">지금 바로 당신의 꿈일기를 시작하세요!</li>
+        <li className="list-item">
+          여러분의 독특한 이야기가 다른 사람들에게도 도움이 될 수 있습니다.
+        </li>
+      </ul>
+      {/* <Impimg src={landingImg} alt="img" /> */}
+      {/* <Moon /> */}
+      {/* <Castle /> */}
+      {/* <Intro>
           IntroPage 당신은 어떤 꿈을 꾸셨나요? “아름다운 꿈이었나요, 아니면 조금
           무서웠나요?" "당신의 꿈은 당신만의 이야기입니다. 그 이야기를 우리와
           함께 나누어 보시겠어요?" "지금 바로 당신의 꿈일기를 시작하세요!
           여러분의 독특한 이야기가 다른 사람들에게도 도움이 될 수 있습니다."
         </Intro> */}
-      </Wrap>
     </>
   );
 };
