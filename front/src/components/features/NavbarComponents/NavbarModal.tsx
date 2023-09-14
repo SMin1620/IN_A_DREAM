c0a41cae84a3697b231170c4350f03d3a7374dab
정수완 <krkdhs6240@naver.com>
2023-09-15 01:08:05 +0900

    fix: 네브 모달창 slidespan 컴포넌트 이름 오류 수정

--- front/src/components/features/NavbarComponents/NavbarModal.tsx
+++ front/src/components/features/NavbarComponents/NavbarModal.tsx
@@ -2,43 +2,43 @@ import React from "react";
 import "./styles/NavbarModal.css";
 import Box from "../../common/Box";
 import { isModalOpen } from "../../../types/index";
-import { SlideLeftSpan, SlideRightSpan } from "../../common/SlideSpan";
+import { SlideSpan } from "../../common/SlideSpan";
 const NavbarModal: React.FC<isModalOpen> = ({ isNavbarModalOpen, onClose }) => {
   return (
     <Box position="absolute" width={80} height={70} left={0} top={15}>
       <div className="navbar-gallery">
-        {/* <SlideLeftSpan
+        {/* <SlideSpan
           fontSize="2.5rem"
           margin="1%"
           speed={5}
           fontFamily="PartialSansKR-Regular"
         >
           GALLERY
-        </SlideLeftSpan>
-        <SlideLeftSpan
+        </SlideSpan>
+        <SlideSpan
           fontSize="2.5rem"
           margin="1%"
           speed={5}
           fontFamily="omyu_pretty"
         >
           GALLERY
-        </SlideLeftSpan>
-        <SlideLeftSpan
+        </SlideSpan>
+        <SlideSpan
           fontSize="2.5rem"
           margin="1%"
           speed={5}
           fontFamily="IAMAPLAYER"
         >
           GALLERY
-        </SlideLeftSpan>
-        <SlideLeftSpan
+        </SlideSpan>
+        <SlideSpan
           fontSize="2.5rem"
           margin="1%"
           speed={5}
           fontFamily="OKDDUNG"
         >
           GALLERY
-        </SlideLeftSpan> */}
+        </SlideSpan> */}
       </div>
     </Box>
   );
