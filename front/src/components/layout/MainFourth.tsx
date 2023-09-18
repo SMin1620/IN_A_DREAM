9ae0ed2ef4780844ce37498803942426d36f6c14
김동현 <kdhdh001@naver.com>
2023-09-18 17:59:05 +0900

    faet: imgslide 수정중

--- front/src/components/layout/MainFourth.tsx
+++ front/src/components/layout/MainFourth.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles/MainFourth.css";
+import ImageSlide from "../features/ImgSlide/ImgSlide";
 
+const itemData = [
+  "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+  "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+  "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+  "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+  "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+  "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+];
 const MainFourth = () => {
   return (
     <div className="MainFourth">
@@ -8,6 +17,7 @@ const MainFourth = () => {
         <h1>COME SEE OUR USERS</h1>
         <h1>DREAM STATISCTICS</h1>
         <h5>네번째 페이지 상점페이지입니다요</h5>
+        <ImageSlide images={itemData} />
       </div>
     </div>
   );
