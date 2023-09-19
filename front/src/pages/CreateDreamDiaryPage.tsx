6d9da6f9cb2ad997fc3d87b0dd51307482d36c47
정수완 <krkdhs6240@naver.com>
2023-09-19 18:01:04 +0900

    design: 꿈 일기 작성페이지 css 작업

--- front/src/pages/CreateDreamDiaryPage.tsx
+++ front/src/pages/CreateDreamDiaryPage.tsx
@@ -4,14 +4,22 @@ import Input from "../components/common/Input";
 import Navbar from "../components/features/NavbarComponents/Navbar";
 import "./styles/CreateDreamDiaryPage.css";
 import Toggle from "../components/common/Toggle";
+import Button2 from "../components/common/Button2";
 
 const CreateDreamDiaryPage = () => {
   const [sell, setSell] = useState<boolean>(false);
-  const [diaryImage, setDiaryImage] = useState(false);
+  const [diaryImage, setDiaryImage] = useState<boolean>(false);
+  const [mainText, setMainText] = useState<string>("");
 
   const inputTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
   };
+
+  const handleMainText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMainText(e.target.value);
+  };
+
+  console.log(mainText);
 
   const handleCreateImage = () => {
     // 로직처리 후 다이어리 이미지 넣어주기
@@ -35,7 +43,10 @@ const CreateDreamDiaryPage = () => {
             <Toggle />
             <Toggle />
           </div>
-          <textarea className="create-dream-diary-textarea" />
+          <textarea
+            onChange={(e) => handleMainText(e)}
+            className="create-dream-diary-textarea"
+          />
           <button onClick={handleCreateImage}>그림생성</button>
         </div>
       </div>
