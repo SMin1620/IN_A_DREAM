af0193f33f1faf8dacbeb4f15b9d46682ade3865
정수완 <krkdhs6240@naver.com>
2023-09-20 18:01:18 +0900

    design: 꿈일기 작성 반응형 css 완성

--- front/src/components/common/Toggle.tsx
+++ front/src/components/common/Toggle.tsx
@@ -1,43 +1,57 @@
 import React, { useState } from "react";
 import S from "styled-components";
 
+interface TogglePorps {
+  AbleColor: string;
+  DisableColor: string;
+}
+
 const Button = S.button`
-  width: 5rem;
-  height: 4rem;
-  font-size: 2rem;
+  width: 4vw;
+  height: 5vh;
+  font-size: calc(1rem + 0.5vw);
   text-align: center;
   border: none;
+  margin: 1rem 0;
+
+  @media (max-width:1024px) {
+    width:7vw; 
+    height:5vh; 
+    font-size:calc(1rem + 0.5vw);
+  }
 `;
 
 const OnButton = S(Button)`
   border-radius: 1rem 0 0 1rem;
+  margin-left: 1rem
 `;
 
 const OffButton = S(Button)`
   border-radius: 0 1rem 1rem 0;
+  margin-right: 1rem;
 `;
 
-const Able = {
-  border: "none",
-  backgroundColor: "#e9c980",
-  color: "white",
-};
-
-const Disable = {
-  backgroundColor: "#c3baa5",
-  color: "white",
-};
-
-const Toggle = () => {
+const Toggle = ({ AbleColor, DisableColor }: TogglePorps) => {
   const [sell, setSell] = useState(false);
+  const Able = {
+    border: "none",
+    backgroundColor: AbleColor,
+    color: "white",
+  };
+
+  const Disable = {
+    border: "none",
+    backgroundColor: DisableColor,
+    color: "white",
+  };
 
   return (
     <div>
       <OnButton style={sell ? Able : Disable} onClick={() => setSell(true)}>
-        on
+        ON
       </OnButton>
       <OffButton style={sell ? Disable : Able} onClick={() => setSell(false)}>
-        off
+        OFF
       </OffButton>
     </div>
   );
--- front/src/pages/CreateDreamDiaryPage.tsx
+++ front/src/pages/CreateDreamDiaryPage.tsx
@@ -1,35 +1,16 @@
 import React, { useState } from "react";
-import Button from "../components/common/Button";
-import Input from "../components/common/Input";
 import Navbar from "../components/features/NavbarComponents/Navbar";
 import "./styles/CreateDreamDiaryPage.css";
-import Toggle from "../components/common/Toggle";
-import Button2 from "../components/common/Button2";
+import CreateDreamDiaryForm from "../components/features/CreateDreamDiary/CreateDreamDiaryForm";
 
 const CreateDreamDiaryPage = () => {
   const [sell, setSell] = useState<boolean>(false);
   const [diaryImage, setDiaryImage] = useState<boolean>(false);
-  const [mainText, setMainText] = useState<string>("");
-
-  const inputTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-  };
-
-  const handleMainText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMainText(e.target.value);
-  };
-
-  console.log(mainText);
-
-  const handleCreateImage = () => {
-    // 로직처리 후 다이어리 이미지 넣어주기
-    setDiaryImage(true);
-  };
 
   return (
-    <div>
+    <div className="create-dream-diary">
       <Navbar />
-      <div className="create-dream-diary">
+      <div className="create-dream-diary-main">
         <div
           className={`create-dream-diary-left ${diaryImage ? "on-diary" : ""}`}
         >
@@ -38,16 +19,7 @@ const CreateDreamDiaryPage = () => {
         </div>
 
         <div className="create-dream-diary-right">
-          <div className="create-dream-diary-right-head">
-            <input type="text" />
-            <Toggle />
-            <Toggle />
-          </div>
-          <textarea
-            onChange={(e) => handleMainText(e)}
-            className="create-dream-diary-textarea"
-          />
-          <button onClick={handleCreateImage}>그림생성</button>
+          <CreateDreamDiaryForm setDiaryImage={setDiaryImage} />
         </div>
       </div>
     </div>
