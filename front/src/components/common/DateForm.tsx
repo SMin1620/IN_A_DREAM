3e012fc1ad3fb5b39765757a557a9aa6d7484290
윤자현 <bajh323@naver.com>
2023-09-21 15:31:23 +0900

    feat: 메인화면 date추가

--- front/src/components/common/DateForm.tsx
+++ front/src/components/common/DateForm.tsx
@@ -1,44 +1,15 @@
 import React from "react";
 import S from "styled-components";
 import { LoginButton } from "../../types/index";
+import DateBox from "./DateBox";
 
-const StyledDate = S.div<LoginButton>`
-background-color: ${(props) => props.backgroundColor}
-height: ${(props) => props.height}
-width: ${(props) => props.width}
-border: ${(props) => props.border}
-margin: ${(props) => props.margin}
-color: ${(props) => props.color}  
-border-radius: ${(props) => props.borderRadius};
-`;
-
-const DateForm: React.FC<LoginButton> = ({
-  width,
-  height,
-  border,
-  backgroundColor,
-  borderRadius,
-  color,
-  margin,
-}) => {
+const DateForm = () => {
   const today = new Date();
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더해줍니다.
   const day = String(today.getDate()).padStart(2, "0");
 
-  return (
-    <StyledDate
-      width={width}
-      height={height}
-      border={border}
-      margin={margin}
-      color={color}
-      backgroundColor={backgroundColor}
-      borderRadius={borderRadius}
-    >
-      {`${year}.${month}.${day}`}
-    </StyledDate>
-  );
+  return <DateBox>{`${year}.${month}.${day}`}</DateBox>;
 };
 
 export default DateForm;
