38725f012e85a4ef9b4f7bde533e26b071b25a38
이승민 <dltmdals1620@gmail.com>
2023-09-21 12:35:55 +0900

    fix: dto 와 엘라스틱 데이터 필드 매핑 오류 해결

--- back/dream/src/main/java/com/dream/dream/elk/dto/DiaryElastic.java
+++ back/dream/src/main/java/com/dream/dream/elk/dto/DiaryElastic.java
@@ -1,5 +1,6 @@
 package com.dream.dream.elk.dto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.ToString;
@@ -16,11 +17,13 @@ public class DiaryElastic {
     @Id
     private String id;
 
-    @Field(type = FieldType.Auto)
+    @Field(type = FieldType.Auto, name = "diary_id")
+    @Column(name = "diary_id")
     private Long diaryId;
 
-    @Field(type = FieldType.Auto)
-    private int member;
+    @Field(type = FieldType.Auto, name = "member_id")
+    @Column(name = "member_id")
+    private int memberId;
 
     @Field(type = FieldType.Auto)
     private String title;
