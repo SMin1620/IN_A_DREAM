00f120875b02a1fdfaa460a11d75006ee9583e2c
김동현 <kdhdh001@naver.com>
2023-09-22 09:47:00 +0900

    feat: login알람, mypage 내일기 조회

--- front/src/api/instances/api1.tsx
+++ front/src/api/instances/api1.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios 인스턴스 생성
 const api1 = axios.create({
-  baseURL: "http://52.78.186.98:8082", // API의 기본 URL
+  baseURL: "http://192.168.30.162:8080/", // API의 기본 URL
 });
 
 // 요청 인터셉터 설정
@@ -11,6 +11,7 @@ api1.interceptors.request.use(
     // 여기에 원하는 요청 전처리 로직을 추가할 수 있습니다.
     // 예를 들어, 토큰을 헤더에 추가하거나 요청 데이터를 수정할 수 있습니다.
     const token = localStorage.getItem("token");
+
     if (token) {
       config.headers["Authorization"] = token;
     }
--- front/src/components/features/DiaryDrrList/DiaryDrrList.tsx
+++ front/src/components/features/DiaryDrrList/DiaryDrrList.tsx
@@ -1,18 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { useMyDiaries } from "../../../hooks/useMyDiary";
-
-export interface Diary {
-  id: number;
-  author: string;
-  title: string;
-  emotion: string[];
-  keyword: string;
-  imageURL: string;
-}
+import { DiaryInfo } from "../../../types/ApiType";
 
 interface DiaryDrrListProps {
-  diaries: Diary[];
+  diaries: DiaryInfo[];
 }
 
 const DiaryContainer = styled.div<{ selected?: boolean }>`
@@ -22,32 +14,22 @@ const DiaryContainer = styled.div<{ selected?: boolean }>`
   position: relative;
   /* transform-origin: top; // To scale from the top of the div */
   transform: ${(props) => (props.selected ? "scaleY(3)" : "scaleY(1)")};
-  /* transition: "transform 0.5s ease-in-out"; */
+  transition: transform 0.1s ease-in-out;
   background-color: blue; // Light blue color: ;
-  margin: 10px;
+  margin: 20px;
+  z-index: ${(props) => (props.selected ? "2" : "1")};
 `;
 
 const Image = styled.img`
   position: absolute;
-  top: "0";
-  left: "0";
-  width: "100%";
-  height: "100%";
+  top: 0;
+  left: 0;
+  width: 30px;
+  height: 30px;
 `;
 
 const DiaryDrrList = ({ diaries }: DiaryDrrListProps) => {
   const [selectedDiaryId, setSelectedDiaryId] = useState<number | null>(null);
-
-  const {
-    data: response,
-    isLoading,
-    error,
-  } = useMyDiaries({ page: 0, size: 1 });
-
-  console.log("response", response);
-  console.log("isLoading", isLoading);
-  console.log("error", error);
-  console.log("datadatadatadatadatadatadatadatadatadata");
 
   return (
     <div>
@@ -59,11 +41,11 @@ const DiaryDrrList = ({ diaries }: DiaryDrrListProps) => {
           selected={selectedDiaryId === diary.id}
         >
           <h3>{diary.title}</h3>
-          <p>{diary.author}</p>
+          {/* <p>{diary.author}</p> */}
 
           {/* Show image only when the diary is selected */}
           {selectedDiaryId === diary.id && (
-            <Image src={diary.imageURL} alt={diary.title} />
+            <Image src={diary.image} alt={diary.title} />
           )}
         </DiaryContainer>
       ))}
