ee7a6dbad832149c83b2a763f6a8f3f325c47c34
이승민 <dltmdals1620@gmail.com>
2023-09-23 18:43:59 +0900

    feat: 일기와 유사한 일기 추천 repository

--- back/dream/src/main/java/com/dream/dream/recommend/repository/ElasticRepository.java
+++ back/dream/src/main/java/com/dream/dream/recommend/repository/ElasticRepository.java
@@ -1,6 +1,6 @@
 package com.dream.dream.recommend.repository;
 
-import com.dream.dream.recommend.dto.DiaryElastic;
+import com.dream.dream.recommend.entity.DiaryElastic;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
@@ -17,6 +17,15 @@ public interface ElasticRepository extends ElasticsearchRepository<DiaryElastic,
 //    @Query("{\"bool\": {\"must\": [{\"term\": {\"member_id\" : \"?0\"}}]}}")
 //    List<DiaryElastic> findAllByMemberId(Long memberId);
 
+    /**
+     * nori 형태소 분석 결과를 검색해서 가져옴
+     */
     @Query("{\"bool\": {\"should\": [{\"match\": {\"content_nori\" : \"?0\"}}, {\"match\": {\"title_nori\" : \"?0\"}}]}}")
     List<DiaryElastic> findByKeyword(String keyword);
+
+    /**
+     * 단어가 포함된 결과를 가져옴
+     */
+    @Query("{\"bool\": {\"should\": [{\"match\": {\"content\" : \"?0\"}}, {\"match\": {\"title\" : \"?0\"}}]}}")
+    List<DiaryElastic> findByDairy(String keyword);
 }
