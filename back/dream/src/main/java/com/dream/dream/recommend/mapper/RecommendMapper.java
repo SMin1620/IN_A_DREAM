6a7810153ec600269589cefb78f007877f673b91
이승민 <dltmdals1620@gmail.com>
2023-09-25 12:07:23 +0900

    feat: 거래시, 생성될 로그 mapper 구현

--- back/dream/src/main/java/com/dream/dream/recommend/mapper/RecommendMapper.java
+++ back/dream/src/main/java/com/dream/dream/recommend/mapper/RecommendMapper.java
@@ -5,6 +5,7 @@ import com.dream.dream.member.dto.MemberDto;
 import com.dream.dream.member.entity.Member;
 import com.dream.dream.recommend.dto.RecommendDto;
 import com.dream.dream.recommend.entity.DiaryElastic;
+import com.dream.dream.transaction.entity.Transaction;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface RecommendMapper {
     List<RecommendDto.DiaryRecommendResponseDto> memberRecommendResponseDto(List<DiaryElastic> diaryElastics);
 
     RecommendDto.DiaryDetailLog recommendLogDto(Diary diary);
+
+    RecommendDto.TransactionLog recommendTransactionLogDto(Transaction transaction);
 }
