c0b3f200948fc07b85eb7c00fd66b0ba30999e51
이승민 <dltmdals1620@gmail.com>
2023-09-26 16:37:18 +0900

    feat: 일별 키워드 통계 dto response 구현

---
+++ back/dream/src/main/java/com/dream/dream/statistic/dto/StatisticDto.java
@@ -0,0 +1,14 @@
+package com.dream.dream.statistic.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StatisticDto {
+    private Long memberId;
+    private String keyword;
+    private int count;
+}
