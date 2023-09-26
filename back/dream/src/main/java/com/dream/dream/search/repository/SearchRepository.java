0cb566ec0da5fbe939067c10e42cde6919bea48d
이승민 <dltmdals1620@gmail.com>
2023-09-26 09:50:13 +0900

    feat: full text search jpa 쿼리 구현

---
+++ back/dream/src/main/java/com/dream/dream/search/repository/SearchRepository.java
@@ -0,0 +1,16 @@
+package com.dream.dream.search.repository;
+
+import com.dream.dream.recommend.entity.DiaryElastic;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface SearchRepository extends ElasticsearchRepository<DiaryElastic, String> {
+
+    /**
+     * 검색
+     */
+    @Query("{\"bool\": {\"should\": [{\"match\": {\"title_nori\" : \"?0\"}}, {\"match\": {\"content_nori\" : \"?0\"}}]}}")
+    List<DiaryElastic> findByDairy(String keyword);
+}
