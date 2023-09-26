a3d255bcd2345593af1da5f9fa46cddff84e4bd7
이승민 <dltmdals1620@gmail.com>
2023-09-26 17:39:11 +0900

    feat: 감정 통계 response dto 구현

--- back/dream/src/main/java/com/dream/dream/statistic/dto/StatisticDto.java
+++ back/dream/src/main/java/com/dream/dream/statistic/dto/StatisticDto.java
@@ -2,13 +2,29 @@ package com.dream.dream.statistic.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class StatisticDto {
-    private Long memberId;
-    private String keyword;
-    private Long count;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class keywordDto {
+        private Long memberId;
+        private String keyword;
+        private Long count;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class emotionDto {
+        private Long memberId;
+        private String emotion;
+        private Long count;
+    }
+
 }
