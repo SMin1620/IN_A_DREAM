68a4d990e3227c90c1f860a9206a49ffb4860dc4
이승민 <dltmdals1620@gmail.com>
2023-09-27 11:02:16 +0900

    feat: 잔디 로그 엘라스틱에 접근해서 와일드 카드 적용 쿼리 구현

--- back/dream/src/main/java/com/dream/dream/statistic/repository/StatisticRepository.java
+++ back/dream/src/main/java/com/dream/dream/statistic/repository/StatisticRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 
 public interface StatisticRepository extends ElasticsearchRepository<DiaryElastic, String> {
 
+    @Query("{\"bool\": {\"must\": [{ \"term\":{\"memberId\":\"?0\"}}, {\"wildcard\": {\"registDate\": {\"value\": \"?1\"}}}]}}")
+    List<Statistic> findByStrict(Long memberId, String registDate);
+
 }
