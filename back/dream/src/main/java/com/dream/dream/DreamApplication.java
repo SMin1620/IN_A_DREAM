1c26c22d6a75d0de41c5131ca39a99507c57af4c
차민준 <whwhdnfl2@naver.com>
2023-09-27 11:13:01 +0900

    feat: 일기 복구

--- back/dream/src/main/java/com/dream/dream/DreamApplication.java
+++ back/dream/src/main/java/com/dream/dream/DreamApplication.java
@@ -2,13 +2,16 @@ package com.dream.dream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@EnableScheduling
 @EnableJpaAuditing
-@EnableAsync
+@EnableAsync(mode = AdviceMode.PROXY, proxyTargetClass = false,
+		order = Ordered.HIGHEST_PRECEDENCE)
 @SpringBootApplication
 public class DreamApplication {
 
