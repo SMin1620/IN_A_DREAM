4edb22eb3a9b8f5382ec988fa445dcd4081bee3f
김동현 <kdhdh001@naver.com>
2023-10-03 17:21:06 +0900

    feat: mainsecond 영상 완

--- front/src/components/layout/MainSecond.tsx
+++ front/src/components/layout/MainSecond.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/MainSecond.css";
 
 const MainSecond = () => {
+  const [scale, setScale] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let scaleValue = window.scrollY / 600;
+      if (scaleValue > 1) scaleValue = 1.35;
+      if (scaleValue < 0.5) scaleValue = 0.5;
+      setScale(scaleValue);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="MainSecond">
-      <div className="VideoBox">
-        <h1>동영상 예정</h1>
-        <h1>동영상 예정</h1>
-        <h1>동영상 예정</h1>
-        <h1>동영상 예정</h1>
-        <h1>움직인다슉슉</h1>
-        <h1>움직인다슉슉</h1>
-        <h1>움직인다슉슉</h1>
-        <h1>동영상 예정</h1>
-        <h1>동영상 예정</h1>
+      <div className="VideoBox" style={{ transform: `scale(${scale})` }}>
+        <video width="100%" height="100%" autoPlay loop muted>
+          <source src="/Video/dreamVideo.mp4" type="video/mp4" />
+        </video>
       </div>
     </div>
   );
