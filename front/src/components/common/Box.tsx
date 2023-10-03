9057664fb0f6559c98ef941ca076e6fcc2facba2
윤자현 <bajh323@naver.com>
2023-10-03 15:10:36 +0900

    feat: 로딩창 구현

--- front/src/components/common/Box.tsx
+++ front/src/components/common/Box.tsx
@@ -10,39 +10,45 @@ interface BoxPropsComponents {
   height: number;
   children?: React.ReactNode;
   textwrap?: string;
+}
+
+interface StyledBoxProps extends BoxPropsComponents {
   open?: boolean;
   ismobile?: boolean;
 }
 
-const StyledBox = S.div<BoxPropsComponents>`
-  position: ${(props) => props.position};
-  top: ${(props) => props.top}%;
-  bottom: ${(props) => props.bottom}%;
-  left: ${(props) => (props.open ? "0" : "-80")}%;
+const StyledBox = S.div.withConfig({
+  shouldForwardProp: (prop) => !["ismobile", "open"].includes(prop),
+})<StyledBoxProps>`
+position: ${(props) => props.position};
+top: ${(props) => props.top}%;
+bottom: ${(props) => props.bottom}%;
+left: ${(props) => (props.open ? "0" : "-80")}%;
 
-  width: ${(props) => (props.ismobile ? "100" : props.width)}%;
-  height: ${(props) => props.height}%;
-  text-wrap: ${(props) => props.textwrap};
+width: ${(props) => (props.ismobile ? "100" : props.width)}%;
+height: ${(props) => props.height}%;
+text-wrap:${(props) => props.textwrap};
 
-  // 모바일
-  transform: ${(props) =>
-    props.ismobile
-      ? `translateY(${props.open ? "-10%" : "-100%"})`
-      : `translateX(${props.open ? "0" : "-100%"})`};
+// 모바일
+transform:${(props) =>
+  props.ismobile
+    ? `translateY(${props.open ? "-10%" : "-100%"})`
+    : `translateX(${props.open ? "0" : "-100%"})`};
 
-  transition: transform 1s ease-in-out;
+transition : transform 1s ease-in-out;
 
-  font-size: 1rem;
-  background-color: #C3BAA5;
-  border-radius: 40px;
-  border: none;
-  color: #646464;
-  z-index: 5;
-  `;
+font-size :1rem ;
+background-color:#C3BAA5 ;
+border-radius :40px ;
+border:none ;
+color:#646464 ;
+z-index :5 ;
 
-const Box = ({ ismobile, ...otherProps }: BoxPropsComponents) => {
+`;
+
+const Box = ({ ismobile, open, ...otherProps }: StyledBoxProps) => {
   return (
-    <StyledBox ismobile={ismobile} {...otherProps}>
+    <StyledBox ismobile={ismobile} open={open} {...otherProps}>
       {otherProps.children}
     </StyledBox>
   );
