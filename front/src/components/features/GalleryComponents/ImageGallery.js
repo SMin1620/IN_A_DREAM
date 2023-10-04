f2c4c83625071fbc79ee80381fb8733eb6d8e8a7
윤자현 <bajh323@naver.com>
2023-10-04 14:32:54 +0900

    fix: 갤러리 수정

--- front/src/components/features/GalleryComponents/ImageGallery.js
+++ front/src/components/features/GalleryComponents/ImageGallery.js
@@ -23,33 +23,29 @@ const GalleryName = {
 const GOLDENRATIO = 1.61803398875;
 
 export const ImageGallery = ({ images, sortKey }) => (
-  <>
-    <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
-      <color attach="background" args={["#191920"]} />
-      <fog attach="fog" args={["#191920", 0, 15]} />
-      <group position={[0, -0.5, 0]}>
-        <Frames images={images} sortKey={sortKey} />
-        <mesh rotation={[-Math.PI / 2, 0, 0]}>
-          <planeGeometry args={[50, 50]} />
-          <>
-            <MeshReflectorMaterial
-              blur={[300, 100]}
-              resolution={2048}
-              mixBlur={1}
-              mixStrength={80}
-              roughness={1}
-              depthScale={1.2}
-              minDepthThreshold={0.4}
-              maxDepthThreshold={1.4}
-              color="#050505"
-              metalness={0.5}
-            />
-          </>
-        </mesh>
-      </group>
-      <Environment preset="city" />
-    </Canvas>
-  </>
+  <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
+    <color attach="background" args={["#191920"]} />
+    <fog attach="fog" args={["#191920", 0, 15]} />
+    <group position={[0, -0.5, 0]}>
+      <Frames images={images} sortKey={sortKey} />
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[50, 50]} />
+        {/* <MeshReflectorMaterial
+          blur={[300, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={80}
+          roughness={1}
+          depthScale={1.2}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#050505"
+          metalness={0.5}
+        /> */}
+      </mesh>
+    </group>
+    <Environment preset="city" />
+  </Canvas>
 );
 
 function Frames({
