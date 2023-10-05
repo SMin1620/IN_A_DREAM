7932c153c91baef585620e6603ff663dcc52aaa8
조현덕 <gusejr4547@gmail.com>
2023-10-05 16:09:01 +0900

    feat: isWrite 초기화 스케줄러

--- back/dream/src/main/java/com/dream/dream/member/repository/MemberRepository.java
+++ back/dream/src/main/java/com/dream/dream/member/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.dream.dream.member.repository;
 import com.dream.dream.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -12,4 +13,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(Long id);
 
     Optional<Member> findByNickname(String nickname);
+
+    List<Member> findMembersByIsWrite(int isWrite);
 }
--- back/dream/src/main/java/com/dream/dream/member/service/MemberService.java
+++ back/dream/src/main/java/com/dream/dream/member/service/MemberService.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -170,8 +171,11 @@ public class MemberService {
     }
 
 
-//    @Scheduled(cron = "0 0 19 * * *")
-//    public void resetMemberIsWrite(){
-//        List<Member>
-//    }
+    @Scheduled(cron = "0 1 * * * *")
+    public void resetMemberIsWrite(){
+        System.out.println("######################스케줄러 발동#######################");
+        List<Member> memberList = memberRepository.findMembersByIsWrite(0);
+        memberList.stream().forEach(member -> member.setIsWrite(1));
+        memberRepository.saveAll(memberList);
+    }
 }
